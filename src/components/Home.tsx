b59d385986a1234b0a2623ef9943d4eb38cc61d2
Dario Hirsch <darohirsch@gmail.com>
2023-11-23 11:36:42 +0100

    feat(home): added link button to board

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,20 @@
+import { useNavigate } from 'react-router-dom'
 import TitleLogo from '../assets/Logo.png'
 
 export const Home = () => {
+	const navigate = useNavigate()
+
+	const handleClick = () => {
+		navigate('/board')
+	}
+
 	return (
 		<div className='w-screen h-screen flex flex-col items-center justify-center bg-customBlue '>
 			<img src={TitleLogo} alt='logo' className='w-1/4' />
 			<h3 className='text-3xl text-white font-inter mb-12'>Test de memoria</h3>
-			<button className='bg-customGreen text-xl text-white py-3 px-6 rounded-md font-inter'>
+			<button
+				onClick={handleClick}
+				className='bg-customGreen text-xl text-white py-3 px-6 rounded-md font-inter'>
 				Comenzar
 			</button>
 		</div>
