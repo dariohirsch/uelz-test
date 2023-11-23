e6841a7cc7c173fc107d0b263617dfa5cd5b9735
Dario Hirsch <darohirsch@gmail.com>
2023-11-23 17:26:18 +0100

    feat(game): added react query to fetch images

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,21 +1,11 @@
 import { useEffect, useState } from 'react'
-import card1 from '../assets/Card1.png'
-import card2 from '../assets/Card2.png'
-import card3 from '../assets/Card3.png'
-import card4 from '../assets/Card4.png'
-import card5 from '../assets/Card5.png'
-import card6 from '../assets/Card6.png'
-import card7 from '../assets/Card7.png'
-import card8 from '../assets/Card8.png'
-import card9 from '../assets/Card9.png'
 import number from '../assets/number1.png'
 import { CustomModal } from '../components/Modal/Modal'
-
-const IMAGES = [card1, card2, card3, card4, card5, card6, card7, card8, card9]
-	.flatMap((image) => [`a-${image}`, `b-${image}`])
-	.sort(() => Math.random() - 0.5)
+import { useQuery } from 'react-query'
+import fetchImages from '../api/api'
 
 export const Board = () => {
+	const { data: IMAGES, isLoading } = useQuery('images', fetchImages)
 	const [selected, setSelected] = useState<string[]>([])
 	const [guessed, setGuessed] = useState<string[]>([])
 	const [isGameWon, setIsGameWon] = useState<boolean>(false)
@@ -38,7 +28,7 @@ export const Board = () => {
 	}, [selected])
 
 	useEffect(() => {
-		if (guessed.length === IMAGES.length) {
+		if (IMAGES && guessed.length === IMAGES.length) {
 			setEndTime(Date.now())
 			setTimeout(() => {
 				setIsGameWon(true)
@@ -51,30 +41,34 @@ export const Board = () => {
 		location.reload()
 	}
 
+	if (isLoading) return <div>Loading...</div>
+
 	return (
 		<div className='bg-customBackground w-screen h-screen flex items-center justify-center '>
 			<div className=' grid grid-cols-6 gap-10'>
-				{IMAGES.map((image) => {
-					const pieces = image.split('-')
-					const url = pieces[1]
-					const isFlipped = selected.includes(image) || guessed.includes(image)
+				{IMAGES &&
+					IMAGES.map((image) => {
+						const pieces = image.split('-')
+						const url = pieces[1]
+						const isFlipped =
+							selected.includes(image) || guessed.includes(image)
 
-					return (
-						<div
-							className={`cursor-pointer ${isFlipped ? 'flip' : ''}`}
-							key={image}
-							onClick={() =>
-								selected.length < 2 &&
-								setSelected((selected) => selected.concat(image))
-							}>
-							{isFlipped ? (
-								<img src={url} alt='card' className='w-40' />
-							) : (
-								<img src={number} alt='card' className='w-40' />
-							)}
-						</div>
-					)
-				})}
+						return (
+							<div
+								className={`cursor-pointer ${isFlipped ? 'flip' : ''}`}
+								key={image}
+								onClick={() =>
+									selected.length < 2 &&
+									setSelected((selected) => selected.concat(image))
+								}>
+								{isFlipped ? (
+									<img src={url} alt='card' className='w-40' />
+								) : (
+									<img src={number} alt='card' className='w-40' />
+								)}
+							</div>
+						)
+					})}
 			</div>
 			<CustomModal
 				handleResetGame={handleResetGame}
