b480495d4c3528759f957ba9899b058a8b55cc84
Dario Hirsch <darohirsch@gmail.com>
2023-11-23 11:35:40 +0100

    feat(game): added board route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './components/Home'
 import './index.css'
+import { Board } from './pages/Board'
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Home />} />
+			<Route path='/board' element={<Board />} />
 		</Routes>
 	)
 }
