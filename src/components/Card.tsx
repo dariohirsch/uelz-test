a117ad0fcdde008264ce4c2c914dc977098cfc72
Dario Hirsch <darohirsch@gmail.com>
2023-11-24 11:43:20 +0100

    fix(cards): changed images quality

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -24,9 +24,9 @@ const Card = ({
 				setSelected((prevSelected) => [...prevSelected, image])
 			}>
 			{isFlipped ? (
-				<img src={url} alt='card' className='w-38' />
+				<img src={url} alt='card' className='w-36' />
 			) : (
-				<img src={numberImage} alt='card' className='w-38' />
+				<img src={numberImage} alt='card' className='w-36' />
 			)}
 		</div>
 	)
