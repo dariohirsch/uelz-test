e2ac30c5307b0776530b600ff85d9b55724516ce
Dario Hirsch <darohirsch@gmail.com>
2023-11-25 11:58:07 +0100

    feat(home): changed title props

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom'
-import TitleLogo from '../assets/main-logo.png'
+import titleLogo from '../assets/main-logo.png'
 import { UelzButton } from '../components/UelzButton'
 
 export const Home = () => {
@@ -11,8 +11,10 @@ export const Home = () => {
 
 	return (
 		<div className='w-screen h-screen flex flex-col items-center justify-center bg-customBlue '>
-			<img src={TitleLogo} alt='logo' className='w-1/4' />
-			<h3 className='text-3xl text-white font-inter mb-12'>Test de memoria</h3>
+			<img src={titleLogo} alt='logo' className='w-1/4' />
+			<h3 className='text-3xl text-customWhite font-inter mb-12'>
+				Test de memoria
+			</h3>
 			<UelzButton onClick={handleClick} text='Comenzar' />
 		</div>
 	)
