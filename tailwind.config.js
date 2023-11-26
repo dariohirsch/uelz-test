df2a028fa0382969d9104c8edb9d596c006bfd7e
Dario Hirsch <darohirsch@gmail.com>
2023-11-26 12:17:35 +0100

    feat(styles): added custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,16 @@ export default {
 		extend: {
 			colors: {
 				customBlue: '#173371',
+				customBackground: '#E9F7F3',
 				customGreen: '#38D4AD',
 				customGradient:
 					'linear-gradient(282deg, #DDFFF6 6.8%, #DEFFF5 21.61%, #FFF9F2 71.37%, #FFF 106.66%)',
 				customWhite: '#FFF',
 				customGrey: '#DBDDE0',
+				customYellow: '#FFFBDE',
+				customBlueback: '#1C80BD',
+				customTeal: '#38D4AD',
+				customGreenback: '#7EECD0',
 			},
 
 			fontFamily: {
